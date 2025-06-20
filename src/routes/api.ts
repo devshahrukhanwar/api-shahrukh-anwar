a5ffd8d7bda995591ddc6b18c0c867de5cd52eb1
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-20 19:58:23 +0530

    Added a new home route for info

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,6 +3,10 @@ import { SocialController } from "../controllers/SocialController.js";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.status(200).json({ status: "API is working" });
+});
+
 // Social media posts
 router.get("/social/posts", SocialController.getPosts);
 
