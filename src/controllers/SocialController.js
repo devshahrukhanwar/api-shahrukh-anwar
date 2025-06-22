4c7589cf68834d4f558c2fa5c6884082a84f14dd
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-23 02:50:09 +0530

    Using both "Dev.to" and "Medium" data and return it as a ombined blogs response

--- src/controllers/SocialController.js
+++ src/controllers/SocialController.js
@@ -1,19 +1,27 @@
-const { SocialMediaService, TwitterService, DevToService } = require("../services");
-// const { SocialMediaService } = require("../services/social/SocialMediaService");
+const {
+  SocialMediaService,
+  TwitterService,
+  DevToService,
+  MediumService,
+} = require("../services");
 
 class SocialController {
   static async getPosts(_, res) {
     try {
       const devto = new SocialMediaService(new DevToService());
+      const medium = new SocialMediaService(new MediumService());
       const twitter = new SocialMediaService(new TwitterService());
       
       const devtoPosts = await devto.fetchPosts();
+      const mediumPosts = await medium.fetchPosts();
       const twitterPosts = await twitter.fetchPosts();
+
+      const blogs = [...mediumPosts, ...devtoPosts].sort((a, b) => new Date(b.date) - new Date(a.date));
 
       res.status(200).send({
         success: true,
         data: {
-          devTo: devtoPosts,
+          blogs,
           twitter: twitterPosts,
         },
       });
