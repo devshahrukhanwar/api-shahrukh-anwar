13374eb4a2ea049322f2b14b0a9b62aa34679872
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-23 02:50:41 +0530

    Added a Medium service to get the Medium blog data

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 const {  SocialMediaService } = require('./social/SocialMediaService');
 const { TwitterService } = require('./social/TwitterService');
 const { DevToService } = require('./social/DevToService');
+const { MediumService } = require('./social/MediumService');
 
-module.exports = { SocialMediaService, TwitterService, DevToService };
+module.exports = { SocialMediaService, TwitterService, DevToService, MediumService };
---
+++ src/services/social/MediumService.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+class MediumService {
+  /**
+   * Get all the recent posts from Medium using the Medium API.
+   * @returns {Promise<void>}
+   * @throws {Error} If the request fails or the API returns an error.
+   */
+  async fetchPosts() {
+    try {
+      const rssFeed = 'https://medium.com/feed/@devshahrukh';
+
+      const { data } = await axios.get(`https://api.rss2json.com/v1/api.json?rss_url=${rssFeed}`);
+
+      return data.items.slice(0, 5).map(post => ({
+        source: 'Medium',
+        title: post.title,
+        // description: post.description,
+        url: post.link,
+        banner: post.thumbnail || this.extractOgImage(post.content),
+        date: post.pubDate,
+        // html: post.content
+      }))
+    } catch (err) {
+      throw new Error(`Failed to fetch posts from Medium: ${err.message}`);
+    }
+  }
+
+  extractOgImage(html) {
+    const match = html.match(/<img[^>]+src="([^">]+)"/)
+    return match?.[1] || ''
+  }
+}
+
+module.exports = { MediumService };
