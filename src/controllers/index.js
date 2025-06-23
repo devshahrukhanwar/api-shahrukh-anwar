38dfca5a9f39cd5cfa1fd4c31ff074b68fd2cb85
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-24 02:51:54 +0530

    Added index file for controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,7 @@
+const { SocialController } = require("./SocialController");
+const { PuppeteerController } = require("./PuppeteerController");
+
+module.exports = {
+  SocialController,
+  PuppeteerController
+};
