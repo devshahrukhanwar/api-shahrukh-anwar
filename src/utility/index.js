f3e1ae71915eb6459064dd03500ffc8e01c0e9d5
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-26 15:39:18 +0530

    Added a Log class to log the data to console and view it on Vercel

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,9 +1,11 @@
 const Cache = require('./cache');
+const Log = require('./log');
 const Response = require('./response');
 const { getHTML } = require('./template');
 
 module.exports = {
   Cache,
+  Log,
   Response,
   getHTML
 };
