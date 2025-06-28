bdfc554acea99769a6428214341a1f99cc4ccaac
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-28 23:55:54 +0530

    Added the OpenAPI swagger documentation to the APIs

---
+++ src/swagger.js
@@ -0,0 +1,32 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My Portfolio API Documentation',
+      version: '1.0.0',
+      description: 'This is the API documentation for my developer portfolio.',
+    },
+    servers: [
+      {
+        url: 'https://api-shahrukhanwar.vercel.app',
+        description: 'Production server',
+      },
+      ...(process.env.APP_ENV === 'local' ? [
+        {
+          url: 'http://localhost:3000',
+          description: 'Local server',
+        },
+      ] : []),
+    ],
+  },
+  apis: ['./src/routes/api.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
